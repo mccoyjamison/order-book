70be272af5a5ff09a132333229d48c141c459ae5
McCoy <8932344+mjamison3@users.noreply.github.com>
2019-10-21 19:27:26 -0400

    commited with lots of if statesments that passes the test but need to be updated to handle more than 2 items in the array

--- orderBook.js
+++ orderBook.js
@@ -1,5 +1,35 @@
-function reconcileOrder(existingBook, incomingOrder) {
-  throw new Error('Not Implemented Yet')
+function reconcileOrder(existingBook, incomingOrders) {
+  
+  if (existingBook.length === 0) {
+    return [incomingOrders]
+  }
+
+  updatedBook = existingBook.concat([incomingOrders])
+   
+  let exBook = updatedBook[0]
+  let inOrder = updatedBook[1]
+
+  if (exBook.type !== inOrder.type && exBook.quantity === inOrder.quantity && exBook.price >= inOrder.price) {
+    return []
+  }
+
+  if (exBook.type !== inOrder.type && exBook.quantity !== inOrder.quantity && exBook.price === inOrder.price) {
+    if (exBook.quantity - inOrder.quantity >= 1) {
+      return [{
+        type: 'buy',
+        quantity: exBook.quantity - inOrder.quantity,
+        price: exBook.price,
+      }]
+    } else if (inOrder.quantity - exBook.quantity >= 1) {
+       return [{
+         type: 'sell',
+         quantity: inOrder.quantity - exBook.quantity,
+         price: exBook.price,
+       }]
+     }
+  }
+  return updatedBook 
 }
+
 
 module.exports = reconcileOrder
